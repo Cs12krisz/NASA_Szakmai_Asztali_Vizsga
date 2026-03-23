74b6d2023873d0cbb5be3a9035912cde12e4a61d
Cs12krisz <csigerkrisztian@gmail.com>
2026-03-23 10:12:26 +0100

    Projekt elmentése

---
+++ NASACLI/Models/Kuldetes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NASACLI.Models
+{
+    public class Kuldetes
+    {
+        public string Nev { get; private set; }
+        public int Ev { get; private set; }
+        public string Celpont { get; private set; }
+        public int Legenyseg { get; private set; }
+        public bool Sikeres { get; private set; }
+        public string Leiras { get; private set; }
+        public double Koltseg { get; private set; }
+        public double HasznosTeher { get; private set; }
+
+        public Kuldetes(string sor) 
+        {
+            string[] s = sor.Split(';');
+            Nev = s[0];
+            Ev = int.Parse(s[1]);
+            Celpont = s[2];
+            Legenyseg = int.Parse(s[3]);
+            Sikeres = s[4] == "Igen" ? true : false;
+            Leiras = s[5];
+            Koltseg = double.Parse(s[6]);
+            HasznosTeher = double.Parse(s[7]);
+        }
+    }
+}
---
+++ NASACLI/Program.cs
@@ -0,0 +1,36 @@
+﻿using NASACLI.Models;
+
+namespace NASACLI
+{
+    internal class Program
+    {
+        public static List<Kuldetes> kuldetesek = new List<Kuldetes>();
+        static void Main(string[] args)
+        {
+            Beolvasas();
+            Feladat3();
+            Feladat4();
+        }
+
+        private static void Feladat4()
+        {
+            Console.WriteLine();
+        }
+
+        private static void Feladat3()
+        {
+            Console.WriteLine($"3. feladat: {kuldetesek.Count} küldetés található az állományban.");
+        }
+
+        public static void Beolvasas()
+        {
+            StreamReader streamReader = new StreamReader("NASAmissions.txt");
+            streamReader.ReadLine();
+            while (!streamReader.EndOfStream) 
+            {
+                Kuldetes kuldetes = new Kuldetes(streamReader.ReadLine());
+                kuldetesek.Add(kuldetes);
+            }
+        }
+    }
+}
